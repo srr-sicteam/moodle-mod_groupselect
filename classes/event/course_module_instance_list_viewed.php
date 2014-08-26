8ad81b55e1c7e8ef0e8028bb0dbede2b275e9b76
Jim Thorpe <jim.thorpe@canterbury.ac.nz>
2014-08-26 12:01:12 +1200

    Implemented new version for 'view all' logging

---
+++ classes/event/course_module_instance_list_viewed.php
@@ -0,0 +1,10 @@
+<?php
+namespace mod_groupselect\event;
+defined('MOODLE_INTERNAL') || die();
+class course_module_instance_list_viewed extends \core\event\course_module_instance_list_viewed {
+    protected function init() {
+        $this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['objecttable'] = 'groupselect';
+    }
+}
