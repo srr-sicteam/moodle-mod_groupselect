e5f67086873b2bc9a6c24db3819c1fb13815382b
Pirkka Pyykkönen <pirkka.pyykkonen@tut.fi>
2014-11-17 14:58:28 +0200

    updated required moodle version

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $module->version   = 2014111000; // The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2011070101; // Requires this Moodle version
+$module->requires  = 2014041100; // Requires this Moodle version
 $module->cron      = 0;          // Period for cron to check this module (secs)
 $module->component = 'mod_groupformation'; // Full name of the plugin (used for diagnostics)
 
