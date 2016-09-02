79e8a4987608bcf8d27fb163e67f2154552caaef
barrasroger <roger.barras@htwchur.ch>
2016-09-02 09:24:42 +0200

    beta info removed from module help intro

--- lang/de/groupselect.php
+++ lang/de/groupselect.php
@@ -2,6 +2,7 @@
 
 $string['eventgroupteacheradded'] = 'Trainer ohne Bearbeitungsrecht zugeordnet';
 $string['eventexportlinkcreated'] = 'Exportlink erstellt';
+$string['modulename_help'] = '<p>Teilnehmer/innen können Gruppen erstellen und wählen: </p><ul><li>Teilnehmer/innen können Gruppen erstellen, diesen eine Beschreibung geben und, falls gewünscht, mit einem Passwort schützen</li><li>Teilnehmer/innen können Gruppen auswählen und betreten</li><li>Trainer/innen ohne Bearbeitungsrechte können Gruppen hinzugefügt werden</li><li>Trainer/in kann die Gruppenliste des Kurses als CSV-Datei exportieren</li><li>Volle Kompatibilität zu Basis Moodle-Gruppen: Gruppen können auch durch andere Plugins erzeugt werden.</li></ul>';
 $string['notifyexpiredselection'] = 'Zeige Meldung, wenn das Einschreibeende vorüber ist';
 $string['notifyexpiredselection_help'] = 'Wenn gesetzt, wird eine Meldung angezeigt falls das Einschreibeende vorüber ist';
 $string['studentcansetenrolmentkey'] = 'Teilnehmende können Passwörter setzen um Gruppen beizutreten';
