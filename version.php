7513abe65459e47d88201196f8fc7aba7bbf04d0
Rog <barrasroger@users.noreply.github.com>
2017-02-01 11:49:34 +0100

    Verions update

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016090800; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2017020100; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100; // Requires this Moodle version
 $plugin->cron      = 0;          // Period for cron to check this module (secs)
 $plugin->component = 'mod_groupselect'; // Full name of the plugin (used for diagnostics)
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "3.11"; // User-friendly version number
+$plugin->release   = "3.12"; // User-friendly version number
